092d9e1b2df93787ff95f4eaaa7f93fbf1de6e24
Bruna Delmouro <brunadelmouro@gmail.com>
2024-03-25 21:23:37 -0300

    API calls in kotlin using retrofit lib - Creating a new project

---
+++ UIFeatures/MyRecipeApp/app/src/main/java/br/com/brunadelmouro/myrecipeapp/ApiService.kt
@@ -0,0 +1,20 @@
+package br.com.brunadelmouro.myrecipeapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+// TODO: defining the default URL and setting up the converter to kotlin objects
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+
+// TODO: creating the retrofit definition
+val recipeService = retrofit.create(ApiService::class.java)
+
+interface ApiService {
+
+    // TODO: 'suspend' keyword is useful to run tasks asynchronous, in separated threads
+    @GET("categories.php")
+    suspend fun getCategories(): CategoriesResponse
+
+}
