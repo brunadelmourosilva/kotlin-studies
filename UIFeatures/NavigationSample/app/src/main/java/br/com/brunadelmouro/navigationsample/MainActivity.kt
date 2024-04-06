4b14fbc315a6daa7ff641d895c88bc0981eb9fcc
Bruna Delmouro <brunadelmouro@gmail.com>
2024-04-06 14:43:01 -0300

    Navigation in Androind - creating two composable screens

---
+++ UIFeatures/NavigationSample/app/src/main/java/br/com/brunadelmouro/navigationsample/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.brunadelmouro.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.brunadelmouro.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FirstScreen()
+                }
+            }
+        }
+    }
+}
